2bdab381adeada526a959864eb4b879adbf377fb
Mickey77100 <bessodem@free.fr>
2025-12-03 18:16:11 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+        
+        
+    
+        <a href="#">
+        <img src="tweeter.png" alt="tweeter"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </a>
+      <a href="#">
+      <img src="insta.jpeg" alt="instagram">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; </a>
+      <a href="#">
+      <img src="facebook.png" alt="facebook"></a>
+       
+      
